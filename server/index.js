ddbd00731bc3041df35c16f2f72515c1a2602c2a
James Meyers <james.meyers919@gmail.com>
2015-06-09 21:29:01 -0700

    Added pm2 module. index.js runs pm2 targeting smartpump.js

--- server/index.js
+++ server/index.js
@@ -1,56 +1,12 @@
-"use strict";
+var pm2 = require("pm2");
 
-/**
- * @file index.js
- * @brief Application entry point
- *
- * Starting point of application.  Also looks for beaglebone and if not found does not initialize relays.
- *
- * @code node index.js
- */
-
-/**
- * @brief Required modules for system startup
- * @type {exports}
- */
-var logger    = require("./logger")("index"),
-    webserver = require("./webserver"),
-    scheduler = require("./pump-scheduler"),
-    db        = require("./database"),
-    pumps     = require("./pumps"),
-    Q         = require( 'q' ),
-    status    = require("./global_status");
-
-
-status.statusInit(); // Initializes the systems global variables and checks if installed on a beaglebone
-
-
-db.connect().then(
-    function() {
-        logger.info( "Successfully connected to mysql database" );
-
-        if( status.onBeagleBone() ){
-            logger.debug("Found beaglebone");
-            
-            try{
-                pumps.init();
-            } catch( e ){
-                logger.error("Fatal Error: " + e);
-            }
-        } else {
-            logger.debug("Not a beaglebone platform, not initializing pumps");
+pm2.connect(function() {
+    pm2.start({
+        script: __dirname + "/smartpump.js"
+    }, function(err, apps) {
+        if(err) {
+            console.log(err.toString());
         }
-
-        return scheduler.init();
-    },
-    function(err) {
-        logger.error("Fatal - " + err );
-        return Q.reject();
-    }
-).then( function() {
-    require("./test-user"); // TODO: Remove test user before we install
-}).then( function(){
-    return webserver.init();
-}, function( err ){
-    logger.debug( "Error: " + err );
+        pm2.disconnect();
+    });
 });
