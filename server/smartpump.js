8adea2bc284f3d8d6d7a1556d30491f9bc5c191f
Richard Lowe <ocdrichard@gmail.com>
2015-07-01 08:40:45 +0000

    Added email alert to initialization

--- server/smartpump.js
+++ server/smartpump.js
@@ -13,6 +13,7 @@
  */
 var logger = require("./logger")("index");
 var webserver = require("./webserver");
+var emailalert = require("./emailalert");
 var configManager = require("./config-manager");
 var tideManager = require("./tide-manager");
 var scheduler = require("./pump-scheduler");
@@ -21,12 +22,23 @@ var pumps = require("./pumps");
 var Q = require("q");
 var status = require("./global-status");
 
+/**
+ * @brief Shortcut for logger function
+ * 
+ * @param type - type of log message ( error, warn, info, debug )
+ * @param message - text to be placed with the logged message
+ * @retval function of logger type
+ */
 function log(type, message) {
     return function() {
         logger[type](message);
     };
 }
 
+/**
+ * @brief Initializes the pumps either on the board or off
+ * 
+ */
 function initializePumps() {
     if(status.onBeagleBone()) {
         logger.info("Beagle bone detected. Initializing pumps");
@@ -36,19 +48,27 @@ function initializePumps() {
     }
 }
 
+/**
+ * @brief Handles fatal errors
+ * 
+ * @param error
+ * 
+ */
 function fatalErrorHandler(error) {
     logger.error("FATAL " + error + ": " + error.stack);
     console.log("\n -- ALERT -- Restart this system once the issue has been resolved...");
 }
+
 
 status.init()
     .then(log("info", "Beagle bone status determined"))
     .then(db.connect)
     .then(log("info", "Successfully connected to mysql server"))
     .then(initializePumps)
-    .then(log("info", "Successfully initialized pumps"))
     .then(webserver.init)
     .then(log("info", "Successfully initialized webserver"))
+    .then(emailalert.init)
+    .then(log("info", "Successfully initialized email alert"))
     .then(function() {
         function run() {
             return scheduler.start().then(function() {
